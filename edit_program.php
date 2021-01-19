fe0a2fc7fe8cd49c239d5f4bcd9a80ebe05af36d
Alanhis <alanhis043@gmail.com>
2021-01-19 16:26:42 +0500

    Исправление ошибок
    Исправлен вывод списка с ID разработчика и Названием языка

--- edit_program.php
+++ edit_program.php
@@ -2,10 +2,11 @@
 <head> <title> Редактирование данных о языке </title> </head>
 <body>
 <?php
+$id=$_GET['id'];
  $link=mysqli_connect("localhost","f0470399_programlanguage","BEpBd03x");
 mysqli_query( $link,'set names utf8');
  mysqli_select_db($link,"f0470399_programlanguage") or die("Нет такой таблицы!");
- $rows=mysqli_query($link,"SELECT iq_language , iq_creator , data_create , 	version , name_program FROM program WHERE id =".$_GET['id']." ");
+ $rows=mysqli_query($link,"SELECT iq_language , iq_creator , data_create , 	version , name_program FROM program WHERE iq =".$_GET['id']." ");
  while ($st = mysqli_fetch_array($rows)) {
 $id=$_GET['id'];
  $name = $st['iq_language'];
@@ -16,18 +17,27 @@ $id=$_GET['id'];
  }
 print "<form action='save_edit.php' metod='get'>";
 print "Название языка(ID языка) ";
-$result=mysqli_query($link, "SELECT iq_language, name_language FROM `language`");
+$result=mysqli_query($link, "SELECT language.iq_language, language.name_language ,program.iq FROM `language` JOIN program on program.iq_language=language.iq_language");
 
 echo "<select name ='iq_language' >";
 while ($row = mysqli_fetch_array($result)) {
-    echo "<option value='" . $row['0'] ."'>" . $row['1'] ." ( ".$row['0']." ) "."</option>";
+
+	if($row['2']!=$id){
+    echo "<option value='" . $row['0'] ."'>" . $row['1'] ." ( ".$row['0']." ) "."</option>";}
+    else{
+    	echo "<option selected value='" . $row['0'] ."'>" . $row['1'] ." ( ".$row['0']." ) "."</option>";
+    }
 }
 echo "</select>";
 print "<br>ID Разработчика ";
-$result1=mysqli_query($link, "SELECT iq_creator, name_creator FROM `creator`");
+$result1=mysqli_query($link, "SELECT creator.iq_creator, creator.name_creator,program.iq FROM `creator` JOIN program on program.iq_creator=creator.iq_creator");
 echo "<select name ='iq_creator' >";
 while ($row = mysqli_fetch_array($result1)) {
-    echo "<option value='" . $row['0'] ."'>" . $row['0'] ." - ".$row['1']."</option>";
+	if($row['2']!=$id){
+    echo "<option  value='" . $row['0'] ."'>" . $row['0'] ." - ".$row['1']."</option>";
+	}else{
+	echo "<option selected value='" . $row['0'] ."'>" . $row['0'] ." - ".$row['1']."</option>";
+	}
 }
 echo "</select>";
 print "<br>Дата создания: <input name='data_create' size='20' type='text'
